7d9964423a52789259f64caf2ae21727f40cd28a
Eduardo Bouças <mail@eduardoboucas.com>
2015-07-13 18:55:45 +0100

    v1

--- app.js
+++ app.js
@@ -1,59 +1,189 @@
+/*=====================================
+=            Configuration            =
+=====================================*/
+
+var config = {
+	file: './config',
+	serverPort: 3000,
+	database: './db'
+};
+
+/*-----  End of Configuration  ------*/
+
+/*======================================
+=            Module loading            =
+======================================*/
+
 var express = require('express');
 var app = express();
 var exec = require('child_process').exec;
 var md5 = require('MD5');
+var fs = require('fs');
+var ini = require('ini');
 var bodyParser = require('body-parser');
+var marked = require('marked');
+var subscriptions = require('./lib/subscriptions')(config.database);
+var globalConfig = ini.parse(fs.readFileSync(config.file, 'utf-8'));
+var mailman = require('./lib/mailman')(globalConfig.MAILGUN_KEY, globalConfig.MAILGUN_DOMAIN, globalConfig.MAILGUN_FROM);
+var helpers = require('./lib/helpers');
+
+/*-----  End of Module loading  ------*/
+
+/*============================================
+=            Module configuration            =
+============================================*/
+
+// Request body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function escapeshellarg(arg) {
-  //  discuss at: http://phpjs.org/functions/escapeshellarg/
-  // original by: Felix Geisendoerfer (http://www.debuggable.com/felix)
-  // improved by: Brett Zamir (http://brett-zamir.me)
-  //   example 1: escapeshellarg("kevin's birthday");
-  //   returns 1: "'kevin\\'s birthday'"
+// Markdown parser
+marked.setOptions({
+	renderer: new marked.Renderer(),
+	gfm: true,
+	tables: true,
+	breaks: false,
+	pedantic: false,
+	sanitize: true,
+	smartLists: true,
+	smartypants: false
+});
 
-  var ret = '';
+/*-----  End of Module configuration  ------*/
 
-  ret = arg.replace(/[^\\]'/g, function(m, i, s) {
-    return m.slice(0, 1) + '\\\'';
-  });
 
-  return "'" + ret + "'";
-}
-
-function parseParameter(parameter) {
-	if (!parameter) {
-		return false;
-	}
-
-	parameter = parameter.trim();
-
-	if (!parameter.length) {
-		return false;
-	}
-
-	return escapeshellarg(parameter);
-}
+/**
+*
+* Endpoint: new comment
+*
+**/
 
 app.post('/comments', function (req, res) {
-	var requiredParameters = ['name', 'email', 'message', 'post', 'post-url'];
-	var parsedParameters = {};
+	/**
+	*
+	* Expected data on POST:
+	*
+	* - name: Commenter's name
+	* - email: Commenter's email address
+	* - url: Commenter's website (optional)
+	* - message: Comment
+	* - company: honeypot field (will error if filled in)
+	* - subscribe: Whether to subscribe the commenter to further comments (valid if == 'subscribe')
+	* - post-slug: Post slug
+	* - post-title: Post title
+	* - post-url: Post URL (relative)
+	*
+	**/
+
+	// Check for honeypot
+	if (('company' in req.body) && req.body['company'].length) {
+		res.status(500).send('You\'re not human, go away!');
+		return;
+	}	
+
+	var requiredParameters = ['name', 'email', 'message', 'post-slug', 'post-title', 'post-url'];
+	var parsedData = {};
 	var validated = true;
 
 	requiredParameters.forEach(function (element, index) {
-		parsedParameters[element] = parseParameter(req.body[element]);
-		validated = validated && parsedParameters[element];
+		parsedData[element] = helpers.parseParameter(req.body[element], helpers.sanitize);
+		validated = validated && parsedData[element];
 	});
 
-	//exec(command, function(error, stdout, stderr){ callback(stdout); });
+	// Create date
+	var date = helpers.parseDate(new Date());
+	
+	// Create email hash
+	var emailHash = md5(parsedData['email'].trim().toLowerCase());
 
-	res.send(validated ? 'YES' : 'NO');
+	// Parse message with Markdown
+	var message = marked(parsedData['message']);
+
+	// Prepare shell command
+	var shellCommand = './new-comment.sh';
+	shellCommand += ' --config \'' + config.file + '\'';
+	shellCommand += ' --name \'' + parsedData['name'] + '\'';
+	shellCommand += ' --date \'' + helpers.sanitize(date) + '\'';
+	shellCommand += ' --hash \'' + emailHash + '\'';
+	shellCommand += ' --post \'' + parsedData['post'] + '\'';
+	shellCommand += ' --message \'' + helpers.sanitize(message) + '\'';	
+
+	exec(shellCommand, function (error, stdout, stderr) { 
+		if (error) {
+			res.status(500).send('Failed to add comment.');
+		} else {
+			var response = {
+				hash: emailHash,
+				date: date,
+				message: message
+			};
+
+			res.send(JSON.stringify(response));
+		}
+	});
+
+	// Getting subscribers
+	subscriptions.get(parsedData['post-slug'], function (subscriptionsForPost) {
+		subscriptionsForPost.forEach(function (subscription, index) {
+			// Sending email
+			var data = {
+				title: parsedData['post-title'],
+				slug: parsedData['post-slug'],
+				link: parsedData['post-url'],
+				subscriber: subscription.name,
+				commenter: parsedData['name'],
+				unsubscribe: subscription._id
+			};
+
+			mailman.send('new-comment', subscription.email, data, function (body, error) {
+				if (error) {
+					console.log('[!] Error sending email: ' + error);
+				}
+			});
+		});
+	}, [parsedData['email']]);
+
+	// Subscribe the commenter if necessary
+	if (req.body['subscribe'] === 'subscribe') {
+		var newSubscriber = {
+			name: parsedData['name'],
+			email: parsedData['email'],
+			post: parsedData['post-slug']
+		};
+
+		subscriptions.add(newSubscriber, parsedData['post-slug'], function (addedSubscriber) {
+			if (addedSubscriber) {
+				console.log('[*] Adding subscriber: ' + addedSubscriber.email + '...');
+			}
+		});	
+	}
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+/**
+*
+* Endpoint: unsubscribe from notifications
+*
+**/
 
-  console.log('Example app listening at http://%s:%s', host, port);
+app.get('/unsubscribe/:id', function (req, res) {
+	subscriptions.remove(req.params.id, function (numRemoved) {
+		if (numRemoved > 0) {
+			res.send('Subscription removed!');
+		} else {
+			res.status(404).send('Subscription not found.');
+		}
+	})
+});
+
+/**
+*
+* Server initialization
+*
+**/
+
+var server = app.listen(config.serverPort, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('[*] jekyll-discuss listening at http://%s:%s', host, port);
 });
