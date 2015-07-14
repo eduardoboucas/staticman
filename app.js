a9fbd85d7e5bc493e695183b28876cfd735796e6
Eduardo Bouças <mail@eduardoboucas.com>
2015-07-15 00:58:19 +0100

    Add info to package file and enable CORS

--- app.js
+++ app.js
@@ -50,6 +50,18 @@ marked.setOptions({
 
 /**
 *
+* Middleware: enable CORS
+*
+**/
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+/**
+*
 * Endpoint: new comment
 *
 **/
