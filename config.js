24ddd2636f9fb6483e4611a5e89af29c6146034c
Eduardo Bouças <mail@eduardoboucas.com>
2016-11-22 09:39:27 +0000

    Update API config docs

--- config.js
+++ config.js
@@ -4,32 +4,33 @@ const convict = require('convict')
 
 const schema = {
   env: {
-    doc: 'The applicaton environment',
+    doc: 'The applicaton environment.',
     format: ['production', 'development', 'test'],
     default: 'development',
     env: 'NODE_ENV'
   },
   port: {
-    doc: 'The port to bind',
+    doc: 'The port to bind the application to.',
     format: 'port',
     default: 0,
     env: 'PORT'
   },
   githubToken: {
-    doc: 'GitHub access token',
+    doc: 'Access token to the GitHub account being used to push files with.',
     format: String,
     default: null,
     env: 'GITHUB_TOKEN'
   },
   akistmet: {
     site: {
-      doc: 'Akismet site',
+      doc: 'URL of an Akismet account used for spam checking.',
+      docExample: 'http://yourdomain.com',
       format: String,
       default: null,
       env: 'AKISMET_SITE'
     },
     apiKey: {
-      doc: 'Akismet API key',
+      doc: 'API key to be used with Akismet.',
       format: String,
       default: null,
       env: 'AKISMET_API_KEY'
@@ -37,33 +38,35 @@ const schema = {
   },
   analytics: {
     uaTrackingId: {
-      doc: 'Universal Analytics Analytics account ID',
+      doc: 'Universal Analytics account ID.',
+      docExample: 'uaTrackingId: "UA-XXXX-XX"',
       format: String,
       default: null,
       env: 'UA_TRACKING_ID'
     }
   },
   rsaPrivateKey: {
-    doc: 'RSA private key',
+    doc: 'RSA private key to encrypt sensitive configuration parameters with.',
+    docExample: 'rsaPrivateKey: "-----BEGIN RSA PRIVATE KEY-----\\nkey\\n-----END RSA PRIVATE KEY-----"',
     format: String,
     default: null,
     env: 'RSA_PRIVATE_KEY'
   },
   email: {
     apiKey: {
-      doc: 'Email service API key',
+      doc: 'Mailgun API key to be used for email notifications. Will be overridden by a `notifications.apiKey` parameter in the site config, if one is set.',
       format: String,
       default: null,
       env: 'EMAIL_API_KEY'
     },
     domain: {
-      doc: 'Email service domain',
+      doc: 'Domain to be used with Mailgun for email notifications. Will be overridden by a `notifications.domain` parameter in the site config, if one is set.',
       format: String,
       default: 'staticman.net',
       env: 'EMAIL_DOMAIN'
     },
     fromAddress: {
-      doc: 'Email address to send notifications as',
+      doc: 'Email address to send notifications from. Will be overridden by a `notifications.fromAddress` parameter in the site config, if one is set.',
       format: String,
       default: 'noreply@staticman.net',
       env: 'EMAIL_FROM'
