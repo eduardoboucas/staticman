6a9482ee7406acbd1fec54ac29b9e73f4c328d4b
Zoltan Burgermeiszter <zoltan@burgermeiszter.com>
2017-03-05 23:23:59 +0000

    'origin' bug fix + refactored reCaptcha

--- controllers/process.js
+++ controllers/process.js
@@ -2,45 +2,7 @@
 
 const config = require(__dirname + '/../config')
 const Staticman = require('../lib/Staticman')
-const reCaptcha = require('express-recaptcha')
-
-function checkRecaptcha(staticman, req) {
-  return new Promise((resolve, reject) => {
-    staticman.getSiteConfig().then(siteConfig => {
-      if (!siteConfig.get('reCaptcha.enabled')) {
-        return resolve(false)
-      }
-
-      const reCaptchaOptions = req.body.options && req.body.options.reCaptcha
-
-      if (!reCaptchaOptions || !reCaptchaOptions.siteKey || !reCaptchaOptions.secret) {
-        return reject('Missing reCAPTCHA API credentials')
-      }
-
-      let decryptedSecret
-
-      try {
-        decryptedSecret = staticman.decrypt(reCaptchaOptions.secret)
-      } catch (err) {
-        return reject('Could not decrypt reCAPTCHA secret')
-      }
-
-      if ((reCaptchaOptions.siteKey) !== siteConfig.get('reCaptcha.siteKey') ||
-        (decryptedSecret !== siteConfig.get('reCaptcha.secret'))) {
-        return reject('reCAPTCHA options do not match Staticman config')
-      }
-
-      reCaptcha.init(reCaptchaOptions.siteKey, decryptedSecret)
-      reCaptcha.verify(req, err => {
-        if (err) {
-          return reject(getRecaptchaError(err))
-        }
-
-        return resolve(true)
-      })
-    }).catch(err => reject(err))
-  })
-}
+const Captcha = require('../lib/Captcha')
 
 function createConfigObject(apiVersion, property) {
   let remoteConfig = {}
@@ -56,30 +18,12 @@ function createConfigObject(apiVersion, property) {
   return remoteConfig
 }
 
-function getRecaptchaError(errorCode) {
-  switch (errorCode) {
-    case 'missing-input-secret':
-      return 'reCAPTCHA: The secret parameter is missing'
-
-    case 'invalid-input-secret':
-      return 'reCAPTCHA: The secret parameter is invalid or malformed'
-
-    case 'missing-input-response':
-      return 'reCAPTCHA: The response parameter is missing'
-
-    case 'invalid-input-response':
-      return 'reCAPTCHA: The response parameter is invalid or malformed'
-  }
-
-  return errorCode
-}
-
 function process(staticman, req, res) {
   const ua = config.get('analytics.uaTrackingId') ? require('universal-analytics')(config.get('analytics.uaTrackingId')) : null
   const fields = req.query.fields || req.body.fields
   const options = req.query.options || req.body.options || {}
 
-  return staticman.processEntry(fields, options).then(data => {
+  return staticman.processEntry().then(data => {
     sendResponse(res, {
       redirect: data.redirect,
       fields: data.fields
@@ -92,7 +36,7 @@ function process(staticman, req, res) {
     console.log('** ERR:', err.stack || err, options, fields, req.params)
 
     sendResponse(res, {
-      error: err,
+      error: err.stack,
       errorCode: 'ENTRY_ERROR',
       redirectError: req.body.options.redirectError
     })
@@ -114,7 +58,7 @@ function sendResponse(res, data) {
     return res.redirect(data.redirectError)
   }
 
-  let payload = {
+  const payload = {
     success: !data.error
   }
 
@@ -129,18 +73,19 @@ function sendResponse(res, data) {
 }
 
 module.exports = (req, res, next) => {
-  const staticman = new Staticman(req.params)
+  const staticman = new Staticman(req)
+  const captcha = Captcha.create(staticman)
 
   staticman.setConfigPath(createConfigObject(req.params.version, req.params.property))
   staticman.setIp(req.headers['x-forwarded-for'] || req.connection.remoteAddress)
   staticman.setUserAgent(req.headers['user-agent'])
 
-  return checkRecaptcha(staticman, req).then(usedRecaptcha => {
+  return captcha.checkRecaptcha(req).then(usedRecaptcha => {
     return process(staticman, req, res)
   }).catch(err => {
     return sendResponse(res, {
       error: err,
-      errorCode: 'RECAPTCHA_ERROR',
+      errorCode: 'PROCESSING_ERROR',
       redirect: req.body.options.redirect,
       redirectError: req.body.options.redirectError
     })
