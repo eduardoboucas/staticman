0d4f9c4ac82ee55434b37c804cb7c17894181c05
Gabe Luci <github@luci.ca>
2019-01-07 23:21:47 -0500

    Updated Notification tests and resolved test failures in ErrorHandler.js

--- test/unit/lib/Notification.test.js
+++ test/unit/lib/Notification.test.js
@@ -31,7 +31,7 @@ describe('Notification interface', () => {
 
   test('builds an email message from the template, replacing the placeholders with the data provided', () => {
     const notification = new Notification(mockMailAgent)
-    const message = notification._buildMessage(mockData.fields, mockData.options, mockData.data)
+    const message = notification._buildReplyMessage(mockData.fields, mockData.options, mockData.data)
 
     expect(message.includes(mockData.data.siteName)).toBe(true)
     expect(message.includes(mockData.options.origin)).toBe(true)
@@ -39,14 +39,15 @@ describe('Notification interface', () => {
 
   test('sends an email through the mail agent', () => {
     const notification = new Notification(mockMailAgent)
-    const message = notification._buildMessage(mockData.fields, mockData.options, mockData.data)
+    const message = notification._buildReplyMessage(mockData.fields, mockData.options, mockData.data)
     const recipient = 'john.doe@foobar.com'
     
     notification.send(
       recipient,
       mockData.fields,
       mockData.options,
-      mockData.data
+      mockData.data,
+      'reply'
     )
 
     expect(mockSendFn.mock.calls.length).toBe(1)
