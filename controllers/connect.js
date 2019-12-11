780050ef2a29f7d713bf3d812a7f83283b7a64a9
Alex Waibel <alex@waibel.us>
2019-12-11 13:10:40 -0500

    Remove unnecessary else block.

--- controllers/connect.js
+++ controllers/connect.js
@@ -21,37 +21,37 @@ module.exports = async (req, res) => {
 
   if (isAppAuth) {
     return res.send('OK!')
-  } else {
-    return github.api.repos.listInvitationsForAuthenticatedUser({}).then(({data}) => {
-      let invitationId = null
-
-      const invitation = Array.isArray(data) && data.some(invitation => {
-        if (invitation.repository.full_name === (req.params.username + '/' + req.params.repository)) {
-          invitationId = invitation.id
-
-          return true
-        }
-      })
-
-      if (!invitation) {
-        return res.status(404).send('Invitation not found')
-      }
-
-      return github.api.repos.acceptInvitation({
-        invitation_id: invitationId
-      }).then(response => {
-        res.send('OK!')
-
-        if (ua) {
-          ua.event('Repositories', 'Connect').send()
-        }
-      }).catch(err => { // eslint-disable-line handle-callback-err
-        res.status(500).send('Error')
-
-        if (ua) {
-          ua.event('Repositories', 'Connect error').send()
-        }
-      })
-    })
   }
+
+  return github.api.repos.listInvitationsForAuthenticatedUser({}).then(({ data }) => {
+    let invitationId = null
+
+    const invitation = Array.isArray(data) && data.some(invitation => {
+      if (invitation.repository.full_name === (req.params.username + '/' + req.params.repository)) {
+        invitationId = invitation.id
+
+        return true
+      }
+    })
+
+    if (!invitation) {
+      return res.status(404).send('Invitation not found')
+    }
+
+    return github.api.repos.acceptInvitation({
+      invitation_id: invitationId
+    }).then(response => {
+      res.send('OK!')
+
+      if (ua) {
+        ua.event('Repositories', 'Connect').send()
+      }
+    }).catch(err => { // eslint-disable-line handle-callback-err
+      res.status(500).send('Error')
+
+      if (ua) {
+        ua.event('Repositories', 'Connect error').send()
+      }
+    })
+  })
 }
