050b4001faabb250aa052b9f837532d7b476d0f0
Ron <tikicoder@users.noreply.github.com>
2020-09-18 16:15:51 -0700

    Update connect.js

--- controllers/connect.js
+++ controllers/connect.js
@@ -1,57 +1,207 @@
 'use strict'
 
-const path = require('path')
-const config = require(path.join(__dirname, '/../config'))
-const GitHub = require(path.join(__dirname, '/../lib/GitHub'))
+const config = require('../config')
+const errorHandler = require('./ErrorHandler')
+const { Octokit: GithubApi } = require("@octokit/rest");
+const { App } = require('@octokit/app')
+const { request } = require('@octokit/request')
+const GitService = require('./GitService')
+const Review = require('./models/Review')
+const User = require('./models/User')
 
-module.exports = async (req, res) => {
-  const ua = config.get('analytics.uaTrackingId')
-    ? require('universal-analytics')(config.get('analytics.uaTrackingId'))
-    : null
+const normalizeResponse = ({data}) => data
 
-  const github = await new GitHub({
-    username: req.params.username,
-    repository: req.params.repository,
-    branch: req.params.branch,
-    token: config.get('githubToken'),
-    version: req.params.version
-  })
+class GitHub extends GitService {
+  constructor (options = {}) {
+    super(options.username, options.repository, options.branch)
 
-  const isAppAuth = config.get('githubAppID') && config.get('githubPrivateKey')
+    return (async () => {
+      const isAppAuth = config.get('githubAppID') &&
+        config.get('githubPrivateKey')
+      const isLegacyAuth = config.get('githubToken') &&
+        ['1', '2'].includes(options.version)
 
-  if (isAppAuth) {
-    return res.send('OK!')
+      let authToken
+
+      if (options.oauthToken) {
+        authToken = options.oauthToken
+      } else if (isLegacyAuth) {
+        authToken = config.get('githubToken')
+      } else if (isAppAuth) {
+        authToken = await this._authenticate(options.username, options.repository)
+      } else {
+        throw new Error('Require an `oauthToken` or `token` option')
+      }
+
+      this.api = new GithubApi({
+        auth: `token ${authToken}`,
+        userAgent: 'Staticman',
+        baseUrl: config.get('githubBaseUrl'),
+        request: {
+          timeout: 5000
+        }
+      })
+
+      return this
+    })()
   }
 
-  return github.api.repos.listInvitationsForAuthenticatedUser({}).then(({ data }) => {
-    let invitationId = null
+  async _authenticate (username, repository) {
+    const app = new App(
+      {
+        id: config.get('githubAppID'),
+        privateKey: config.get('githubPrivateKey'),
+        baseUrl: config.get('githubBaseUrl')
+      }
+    )
 
-    const invitation = Array.isArray(data) && data.some(invitation => {
-      if (invitation.repository.full_name === (req.params.username + '/' + req.params.repository)) {
-        invitationId = invitation.id
+    const jwt = app.getSignedJsonWebToken()
 
-        return true
+    const {data} = await request('GET /repos/:owner/:repo/installation', {
+      owner: username,
+      repo: repository,
+      headers: {
+        authorization: `Bearer ${jwt}`,
+        accept: 'application/vnd.github.machine-man-preview+json'
       }
     })
 
-    if (!invitation) {
-      return res.status(404).send('Invitation not found')
+    const installationId = data.id
+
+    let token = await app.getInstallationAccessToken({installationId})
+
+    return token
+  }
+
+  _pullFile (filePath, branch) {
+    return this.api.repos
+      .getContent({
+        owner: this.username,
+        repo: this.repository,
+        path: filePath,
+        ref: branch,
+      })
+      .then(normalizeResponse)
+      .catch((err) =>
+        Promise.reject(errorHandler("GITHUB_READING_FILE", { err }))
+      );
+  }
+
+  _commitFile (filePath, content, commitMessage, branch) {
+    return this.api.repos.createOrUpdateFile({
+      owner: this.username,
+      repo: this.repository,
+      path: filePath,
+      message: commitMessage,
+      content,
+      branch
+    })
+      .then(normalizeResponse)
+  }
+
+  writeFile (filePath, data, targetBranch, commitTitle) {
+    return super.writeFile(filePath, data, targetBranch, commitTitle)
+      .catch(err => {
+        try {
+          const message = err && err.message
+
+          if (message) {
+            const parsedError = JSON.parse(message)
+
+            if (
+              parsedError &&
+              parsedError.message &&
+              parsedError.message.includes('"sha" wasn\'t supplied')
+            ) {
+              return Promise.reject(errorHandler('GITHUB_FILE_ALREADY_EXISTS', {err}))
+            }
+          }
+        } catch (err) {
+          console.log(err)
+        }
+
+        return Promise.reject(errorHandler('GITHUB_WRITING_FILE'))
+      })
+  }
+
+  getBranchHeadCommit (branch) {
+    return this.api.repos.getBranch({
+      owner: this.username,
+      repo: this.repository,
+      branch
+    })
+      .then(res => res.data.commit.sha)
+  }
+
+  createBranch (branch, sha) {
+    return this.api.git.createRef({
+      owner: this.username,
+      repo: this.repository,
+      ref: `refs/heads/${branch}`,
+      sha
+    })
+      .then(normalizeResponse)
+  }
+
+  deleteBranch (branch) {
+    return this.api.git.deleteRef({
+      owner: this.username,
+      repo: this.repository,
+      ref: `heads/${branch}`
+    })
+  }
+
+  createReview (reviewTitle, branch, reviewBody) {
+    return this.api.pullRequests.create({
+      owner: this.username,
+      repo: this.repository,
+      title: reviewTitle,
+      head: branch,
+      base: this.branch,
+      body: reviewBody
+    })
+      .then(normalizeResponse)
+  }
+
+  getReview (reviewId) {
+    return this.api.pulls.get({
+      owner: this.username,
+      repo: this.repository,
+      pull_number: reviewId
+    })
+      .then(normalizeResponse)
+      .then(({base, body, head, merged, state, title}) =>
+        new Review(
+          title,
+          body,
+          (merged && state === 'closed') ? 'merged' : state,
+          head.ref,
+          base.ref
+        )
+      )
+  }
+
+  async readFile (filePath, getFullResponse) {
+    try {
+      return await super.readFile(filePath, getFullResponse)
+    } catch (err) {
+      throw errorHandler('GITHUB_READING_FILE', {err})
     }
+  }
 
-    return github.api.repos.acceptInvitation({
-      invitation_id: invitationId
-    }).then(response => {
-      res.send('OK!')
+  writeFileAndSendReview (filePath, data, branch, commitTitle, reviewBody) {
+    return super.writeFileAndSendReview(filePath, data, branch, commitTitle, reviewBody)
+      .catch(err => Promise.reject(errorHandler('GITHUB_CREATING_PR', {err})))
+  }
 
-      if (ua) {
-        ua.event('Repositories', 'Connect').send()
-      }
-    }).catch(err => { // eslint-disable-line handle-callback-err
-      res.status(500).send('Error')
-
-      if (ua) {
-        ua.event('Repositories', 'Connect error').send()
-      }
-    })
-  })
+  getCurrentUser () {
+    return this.api.users.getAuthenticated({})
+      .then(normalizeResponse)
+      .then(({login, email, avatar_url, name, bio, company, blog}) =>
+        new User('github', login, email, name, avatar_url, bio, blog, company)
+      )
+      .catch(err => Promise.reject(errorHandler('GITHUB_GET_USER', {err})))
+  }
 }
+
+module.exports = GitHub
