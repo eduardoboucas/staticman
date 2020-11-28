ce67be2ca69c9912c9361249c9d4c54c7b6abd82
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-11-28 21:55:09 +0000

    Make encryption acceptance test a little more roboust

--- test/acceptance/encrypt.test.js
+++ test/acceptance/encrypt.test.js
@@ -1,16 +1,23 @@
 import request from 'supertest';
 
+import { decrypt } from '../helpers';
 import StaticmanAPI from '../../source/server';
 
 const staticman = new StaticmanAPI().server;
 const supportedApiVersions = [['v2'], ['v3']];
 
 describe.each(supportedApiVersions)('API %s - Encrypt endpoint', (version) => {
-  it('responds with the text encrypted using the RSA key from the test config', async () => {
+  it('responds with the text query parameter encrypted using the RSA key from the test config', async () => {
     const inputText = 'sometext';
+
+    expect.assertions(2);
+
     await request(staticman)
       .get(`/${version}/encrypt/${inputText}`)
       .expect(200)
-      .expect((response) => expect(response).not.toContain(inputText));
+      .expect((response) => {
+        expect(response.text).not.toContain(inputText);
+        expect(decrypt(response.text)).toBe(inputText);
+      });
   });
 });
