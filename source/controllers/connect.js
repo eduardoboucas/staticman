68a3c7182ca75b2d5835c60ddc29cf2476da1f60
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-11-28 22:46:42 +0000

    Refactor connecct controller

--- source/controllers/connect.js
+++ source/controllers/connect.js
@@ -14,40 +14,42 @@ export default async (req, res) => {
     version: req.params.version,
   });
 
-  const isAppAuth = config.get('githubAppID') && config.get('githubPrivateKey');
-
-  if (isAppAuth) {
+  if (_isAppAuth()) {
     return res.send('Staticman connected!');
   }
 
-  return github.api.repos.listInvitationsForAuthenticatedUser({}).then(({ data }) => {
-    const expectedRepoName = `${req.params.username}/${req.params.repository}`;
+  const { data: invitations } = await github.api.repos.listInvitationsForAuthenticatedUser({});
+  const expectedRepoName = `${req.params.username}/${req.params.repository}`;
 
-    const collaborationInvite =
-      Array.isArray(data) &&
-      data.find((invitation) => invitation.repository.full_name === expectedRepoName);
+  const collaborationInvite = _findMatchingRepoInvitation(invitations, expectedRepoName);
 
-    if (!collaborationInvite) {
-      return res.status(404).send('Invitation not found');
+  if (!collaborationInvite) {
+    return res.status(404).send('Invitation not found');
+  }
+
+  try {
+    await github.api.repos.acceptInvitation({ invitation_id: collaborationInvite.id });
+
+    if (ua) {
+      ua.event('Repositories', 'Connect').send();
     }
 
-    return github.api.repos
-      .acceptInvitation({
-        invitation_id: collaborationInvite.id,
-      })
-      .then(() => {
-        res.send('Staticman connected!');
-
-        if (ua) {
-          ua.event('Repositories', 'Connect').send();
-        }
-      })
-      .catch(() => {
-        res.status(500).send('Error');
-
-        if (ua) {
-          ua.event('Repositories', 'Connect error').send();
-        }
-      });
-  });
+    return res.send('Staticman connected!');
+  } catch {
+    if (ua) {
+      ua.event('Repositories', 'Connect error').send();
+    }
+    return res.status(500).send('Error');
+  }
 };
+
+function _isAppAuth() {
+  return config.get('githubAppID') && config.get('githubPrivateKey');
+}
+
+function _findMatchingRepoInvitation(invitationList, expectedRepoName) {
+  return (
+    Array.isArray(invitationList) &&
+    invitationList.find((invitation) => invitation.repository.full_name === expectedRepoName)
+  );
+}
