2b973907a9babebd7babeb38b5d86797a5bddb43
Eduardo Bouças <mail@eduardoboucas.com>
2020-12-23 11:53:30 +0000

    Revert changes to processEntry in @staticman/netlify-functions

--- packages/netlify-functions/source/index.js
+++ packages/netlify-functions/source/index.js
@@ -1,8 +1,4 @@
 import processEntry from './processEntry';
 
-const processEntryFactory = (configParameters) => {
-  return (...args) => processEntry.apply(this, [configParameters, ...args]);
-};
-
 // eslint-disable-next-line import/prefer-default-export
-export { processEntryFactory as processEntry };
+export { processEntry };
